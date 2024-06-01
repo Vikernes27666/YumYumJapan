b49d84d7ea8c1e8e1c074add19d4b40d8e33a126
Alexxe <4lexxe@gmail.com>
2024-06-01 18:15:38 -0300

    navbar categorias

--- frontend/src/components/navigation/Navbar.jsx
+++ frontend/src/components/navigation/Navbar.jsx
@@ -1,4 +1,8 @@
 import { Fragment } from 'react'
+import logo from './../../assets/Logo.svg';
+import icono from './../../assets/icono.svg';
+import { FaSearch } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
 import { Popover, Transition } from '@headlessui/react'
 import {
   BookmarkAltIcon,
@@ -19,65 +23,9 @@ import {
   ViewGridIcon,
   XIcon,
 } from '@heroicons/react/outline'
+
 import { ChevronDownIcon } from '@heroicons/react/solid'
 
-const solutions = [
-  {
-    name: 'Analytics',
-    description: 'Get a better understanding of where your traffic is coming from.',
-    href: '#',
-    icon: ChartBarIcon,
-  },
-  {
-    name: 'Engagement',
-    description: 'Speak directly to your customers in a more meaningful way.',
-    href: '#',
-    icon: CursorClickIcon,
-  },
-  { name: 'Security', description: "Your customers' data will be safe and secure.", href: '#', icon: ShieldCheckIcon },
-  {
-    name: 'Integrations',
-    description: "Connect with third-party tools that you're already using.",
-    href: '#',
-    icon: ViewGridIcon,
-  },
-]
-const callsToAction = [
-  { name: 'Watch Demo', href: '#', icon: PlayIcon },
-  { name: 'View All Products', href: '#', icon: CheckCircleIcon },
-  { name: 'Contact Sales', href: '#', icon: PhoneIcon },
-]
-const company = [
-  { name: 'About', href: '#', icon: InformationCircleIcon },
-  { name: 'Customers', href: '#', icon: OfficeBuildingIcon },
-  { name: 'Press', href: '#', icon: NewspaperIcon },
-  { name: 'Careers', href: '#', icon: BriefcaseIcon },
-  { name: 'Privacy', href: '#', icon: ShieldCheckIcon },
-]
-const resources = [
-  { name: 'Community', href: '#', icon: UserGroupIcon },
-  { name: 'Partners', href: '#', icon: GlobeAltIcon },
-  { name: 'Guides', href: '#', icon: BookmarkAltIcon },
-  { name: 'Webinars', href: '#', icon: DesktopComputerIcon },
-]
-const blogPosts = [
-  {
-    id: 1,
-    name: 'Boost your conversion rate',
-    href: '#',
-    preview: 'Eget ullamcorper ac ut vulputate fames nec mattis pellentesque elementum. Viverra tempor id mus.',
-    imageUrl:
-      'https://images.unsplash.com/photo-1558478551-1a378f63328e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2849&q=80',
-  },
-  {
-    id: 2,
-    name: 'How to use search engine optimization to drive traffic to your site',
-    href: '#',
-    preview: 'Eget ullamcorper ac ut vulputate fames nec mattis pellentesque elementum. Viverra tempor id mus.',
-    imageUrl:
-      'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2300&q=80',
-  },
-]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -86,18 +34,27 @@ function classNames(...classes) {
 export function Navbar() {
   return (
     <Popover className="relative bg-white">
-      <div className="absolute inset-0 shadow z-30 pointer-events-none" aria-hidden="true" />
+
+<div className="absolute inset-0 shadow z-30 pointer-events-none" aria-hidden="true"></div>
+     
       <div className="relative z-20">
-        <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
-          <div>
-            <a href="#" className="flex">
-              <span className="sr-only">Workflow</span>
-              <img
-                className="h-8 w-auto sm:h-10"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                alt=""
-              />
-            </a>
+      <div className="w-full max-w-[1440px] mx-auto h-[120px] flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
+          <div className="relative z-20">
+            <div className="max-w-[1440px] mx-auto h-[120px] flex justify-between items-center">
+              <div>
+                <a href="#" className="flex">
+                  <img
+                    className="h-14 w-auto sm:h-20"
+                    src={logo}
+                    alt="Logo"
+        />
+          </a>
+        </div>
+        
+         </div>
+
+
+         
           </div>
           
           
@@ -108,119 +65,129 @@ export function Navbar() {
             </Popover.Button>
           </div>
 
-          <div className="hidden md:flex-1 md:flex md:items-center md:justify-between">
-             <div className="relative w-3/4"> {/* Cambia la clase de ancho aquí */}
-      <input
-        type="search"
-        className="block w-full px-4 py-2 border border-gray-300 rounded-md bg-gray-200 focus:ring-indigo-500 focus:border-indigo-500"
-        placeholder="Buscar..."
-        aria-label="Buscar"
-      />
-      <button type="submit" className="absolute right-0 top-0 mt-2 mr-3 ">
-        {/* Icono de búsqueda (puedes colocar aquí tu SVG) */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 text-gray-400"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M15 11a4 4 0 11-8 0 4 4 0 018 0z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M17.5 17.5l4.5 4.5"
-          />
-        </svg>
-      </button>
-    </div>
 
-      <div className="flex items-center ml-10">
-  <a href="#" className="text-base hover:text-gray-900">
+
+ {/* Barra de búsqueda para pantallas medianas y grandes */}
+          <div className="hidden md:flex-1 md:flex md:items-center md:justify-between">
+            <div className="relative md:w-[64%]">
+              <input
+                type="search"
+                className="block w-full px-4 py-3 border border-green-600 rounded-full focus:ring-green-500 focus:border-green-500 outline-none text-green-600 placeholder-green-600"
+                placeholder="¿Qué estás buscando?"
+                aria-label="Buscar"
+              />
+              <button type="submit" className="absolute right-4 top-1/2 transform -translate-y-1/2">
+              <FaSearch />
+              </button>
+            </div>
+<div className="flex items-center ml-2 mx-14 font-montserrat font-medium">
+  
+  <a href="#" className="text-base text-lg hover:text-gray-900 text-red-500">
     Iniciar sesión 
   </a>
-  <span className="mx-3">|</span>
+  <span className="mx-4 text-red-500">|</span>
   <a
     href="#"
-    className="text-base hover:text-gray-900"
+    className="text-base font text-lg hover:text-gray-900 text-red-500"
   >
     Registrarse
   </a>
-  <a href="#" className="ml-5 text-gray-500 hover:text-gray-900">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        d="M3 3h2l1 3h13a1 1 0 0 1 .95 1.32l-3 10A1 1 0 0 1 16 18H7a1 1 0 0 1-.95-.68L3.1 5H3V3zm1 2h.6l2.25 7.5h9.55L17.9 5H4zM6 19a2 2 0 1 0 4 0 2 2 0 0 0-4 0zm8 0a2 2 0 1 0 4 0 2 2 0 0 0-4 0z"
-      />
-    </svg>
-  </a>
+<a href="#" className="ml-10 text-gray-500 hover:text-gray-900">
+<div className="flex items-center justify-center w-10 h-10" style={{ backgroundColor: '#54ACB6', borderRadius: '50%' }}>
+<FaShoppingCart />
+
+  </div>
+</a>
+
+</div>
 </div>
 
-          </div>
+
+      </div>
+
+        <div className="hidden md:flex justify-center bg-red-500 text-white font-montserrat font-medium overflow-x-auto">
+      <div className="px-8 py-2 flex items-center space-x-8">
+    <a href="#" className="">SNACKS</a>
+    <span className=" ">|</span>
+    <a href="#" className="">COMIDA INSTANTÁNEA</a>
+    <span className=" ">|</span>
+    <a href="#" className="">GOLOSINAS</a>
+    <span className=" ">|</span>
+    <a href="#" className="">CHOCOLATES</a>
+    <span className=" ">|</span>
+    <a href="#" className="">GALLETAS</a>
+    <span className=" ">|</span>
+    <a href="#" className="">BEBIDAS</a>
+    <span className=" ">|</span>
+    <a href="#" className="">COMBOS BOX</a>
+  </div>
         </div>
       </div>
 
       <Transition
-        as={Fragment}
-        enter="duration-200 ease-out"
-        enterFrom="opacity-0 scale-95"
-        enterTo="opacity-100 scale-100"
-        leave="duration-100 ease-in"
-        leaveFrom="opacity-100 scale-100"
-        leaveTo="opacity-0 scale-95"
-      >
-        <Popover.Panel
-          focus
-          className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-        >
-          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-            <div className="pt-5 pb-6 px-5 sm:pb-8">
-              <div className="flex items-center justify-between">
-                <div>
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                    alt="Workflow"
-                  />
-                </div>
-                <div className="-mr-2">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <span className="sr-only">Close menu</span>
-                    <XIcon className="h-6 w-6" aria-hidden="true" />
-                  </Popover.Button>
-                </div>
-              </div>
-             
-            </div>
-            <div className="py-6 px-5">
-              <div className="mt-6">
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                >
-                  Sign up
-                </a>
-                <p className="mt-6 text-center text-base font-medium text-gray-500">
-                  Existing customer?{' '}
-                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
-                    Sign in
-                  </a>
-                </p>
-              </div>
-            </div>
+  as={Fragment}
+  enter="duration-200 ease-out"
+  enterFrom="opacity-0 scale-95"
+  enterTo="opacity-100 scale-100"
+  leave="duration-100 ease-in"
+  leaveFrom="opacity-100 scale-100"
+  leaveTo="opacity-0 scale-95"
+>
+  <Popover.Panel
+    focus
+    className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+  >
+    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+      <div className="pt-5 pb-6 px-5 sm:pb-8">
+        <div className="flex items-center justify-between">
+          <div>
+          <img
+          className="h-10"
+          src={icono} 
+          alt="icono"
+        />
           </div>
-        </Popover.Panel>
-      </Transition>
+          <div className="-mr-2">
+            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="sr-only">Close menu</span>
+              <XIcon className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+        </div>
+        <div className="py-6 px-5">
+          <div className="mt-6">
+            <a
+              href="#"
+              className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+            >
+              Sign up
+            </a>
+            <p className="mt-6 text-center text-base font-medium text-gray-500">
+              Existing customer?{' '}
+              <a href="#" className="text-indigo-600 hover:text-indigo-500">
+                Sign in
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Categorías */}
+      <div className="py-4 px-5">
+        <h3 className="text-sm font-semibold text-gray-600 uppercase mb-2">Categorías</h3>
+        <div className="flex flex-col space-y-2">
+          <a href="#" className="text-gray-700 hover:text-gray-900">Snacks</a>
+          <a href="#" className="text-gray-700 hover:text-gray-900">Comida Instantánea</a>
+          <a href="#" className="text-gray-700 hover:text-gray-900">Golosinas</a>
+          <a href="#" className="text-gray-700 hover:text-gray-900">Chocolates</a>
+          <a href="#" className="text-gray-700 hover:text-gray-900">Galletas</a>
+          <a href="#" className="text-gray-700 hover:text-gray-900">Bebidas</a>
+          <a href="#" className="text-gray-700 hover:text-gray-900">Combos box</a>
+        </div>
+      </div>
+    </div>
+  </Popover.Panel>
+</Transition>
+
     </Popover>
   )
 }
