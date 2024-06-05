b608d218a0b11c2a075a990227ebfe05ee61bea9
Alexxe <4lexxe@gmail.com>
2024-06-05 01:28:32 -0300

    correcciones en el navbar

--- src/components/SwiperSlide.jsx
+++ src/components/SwiperSlide.jsx
@@ -1,17 +1,23 @@
+// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
+
+// Import Swiper modules
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+
 import banner1 from '../assets/banner1.png';
 import banner2 from '../assets/banner2.png';
 import banner3 from '../assets/banner3.png';
 
-export default function Carousel() {
+export default () => {
   return (
     <Swiper
-      className='w-full max-w-screen-xl mx-auto'
+      className='w-full max-w-screen-xl mx-auto sm:w-[1440px] sm:h-[360px]'
       modules={[Navigation, Pagination, Autoplay]}
       spaceBetween={50}
       slidesPerView={1}
@@ -21,18 +27,13 @@ export default function Carousel() {
       scrollbar={{ draggable: true }}
       navigation
       autoplay={{ delay: 3000 }}
-      loop={true}
+      loop={true} // Habilitar el bucle infinito
     >
-      {/* Utiliza clases de Tailwind CSS para hacer que las imágenes sean responsivas */}
-      <SwiperSlide>
-        <img src={banner1} alt="Banner 1" className="w-full h-auto sm:h-[350px]" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={banner2} alt="Banner 2" className="w-full h-auto sm:h-[350px]" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={banner3} alt="Banner 3" className="w-full h-auto sm:h-[350px]" />
-      </SwiperSlide>
+      <SwiperSlide><img src={banner1} alt="Banner 1" className="banner-image" /></SwiperSlide>
+      <SwiperSlide><img src={banner2} alt="Banner 2" className="banner-image" /></SwiperSlide>
+      <SwiperSlide><img src={banner3} alt="Banner 3" className="banner-image" /></SwiperSlide>
+
+
     </Swiper>
   );
-}
+};
