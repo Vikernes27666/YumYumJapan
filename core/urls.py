da087c93184b7999e1c1e5ae43c753c05f955361
Esteban Hirzfeld <estebanhirzfeld@gmail.com>
2024-06-05 21:11:16 -0300

    fix: urlpatterns, no es necesario TemplateView con rest framework

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+# urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
