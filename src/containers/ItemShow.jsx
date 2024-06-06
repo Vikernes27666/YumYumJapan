49ee3dc36d323c6e9318d4c535eadde33e9ba8ff
Diego <Diego.iribe@outlook.es>
2024-06-06 10:44:22 -0700

    arreglo de overflow-x

--- src/containers/ItemShow.jsx
+++ src/containers/ItemShow.jsx
@@ -5,7 +5,7 @@ import Carrusel from '../components/ItemShow/Carrusel'
 const ItemShow = () => {
   return (
     <Layout>
-      <div className="w-screen flex flex-col justify-center items-center mt-5">
+      <div className=" flex flex-col justify-center items-center mt-5 overflow-x-hidden ">
         <ItemView />
         <div className="bg-neutral-400 w-[80%] h-[3px] mt-24 m-5"></div>
         <Carrusel />
