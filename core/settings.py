b1173a578dd75a3cf5d0840bce1dbd7354410023
Esteban Hirzfeld <estebanhirzfeld@gmail.com>
2024-06-05 22:39:55 -0300

    feat: documentacion + endopoints de authenticacion

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from pathlib import Path
 import os
 import environ
@@ -7,7 +8,7 @@ env = environ.Env()
 environ.Env.read_env(env_file=".envs/.local/.django")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-ROOT_DIR = Path(__file__).resolve().parent.parent.parent
+ROOT_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,7 +25,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -32,6 +34,25 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+LOCAL_APPS = [
+    "apps.users",
+]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+    "drf_yasg",
+    "corsheaders",
+    "rest_framework.authtoken",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
+    "taggit",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 
 
@@ -107,6 +128,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = "users.User"
+
 
 STATIC_URL = "/staticfiles/"
 STATIC_ROOT = str(ROOT_DIR / "staticfiles")
@@ -119,10 +142,66 @@ MEDIA_ROOT = str(ROOT_DIR / "mediafiles")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# REST_FRAMEWORK
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
     ],
 }
 
-CORS_ORIGIN_WHITELIST = 'http://localhost:5153'
+# SIMPLE_JWT
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "SIGNING_KEY": env("SIGNING_KEY"),
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+}
+
+# REST_AUTH
+REST_AUTH = {
+    "USE_JWT": True,
+    # TODO: Change "Core"
+    "JWT_AUTH_COOKIE": "core-access-token",
+    "JWT_AUTH_REFRESH_COOKIE": "core-refresh-token",
+    "REGISTER_SERIALIZER": "apps.users.serializers.CustomRegisterSerializer",
+}
+
+
+AUTHENTICATION_BACKENDS = [
+    "allauth.account.auth_backends.AuthenticationBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
+
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_USERNAME_REQUIRED = False
+
+ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:5153",
+    "http://localhost:5153",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:5153",
+    "http://localhost:5153",
+]
+
+SESSION_COOKIE_SAMESITE = None
+SESSION_COOKIE_SECURE = False
+CORS_ALLOW_CREDENTIALS = True
+# CORS_ORIGIN_WHITELIST = 'http://localhost:5153'
--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,42 @@ from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+from apps.users.views import CustomUserDetailsView
+from dj_rest_auth.views import PasswordResetConfirmView
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="YumYum Japan",
+        default_version="v1.0",
+        description="YumYum Japan endpoints",
+        contact=openapi.Contact(email="contacto@yumyumjapan.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    # Documentation
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
+
+    # Admin Panel
     path('admin/', admin.site.urls),
+
+    # User
+    path("api/v1/auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("api/v1/auth/user/", CustomUserDetailsView.as_view(), name="user_details"),
+    path("api/v1/auth/", include("dj_rest_auth.urls")),
+    path(
+        "api/v1/auth/password/reset/confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
 ]
 
 # urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
