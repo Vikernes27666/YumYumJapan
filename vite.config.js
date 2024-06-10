be88a1f86361328ef6f992089efebaa042a0a206
vikernes <armic27666@gmail.com>
2024-06-10 15:46:09 -0500

    antiguo vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: './',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './index.html'
-    }
-  }
-});
-
+})
